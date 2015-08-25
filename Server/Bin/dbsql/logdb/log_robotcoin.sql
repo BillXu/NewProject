a63210717934a7662c72e1b01d7b259467d776bd
BillForServer <xuwenyanbill@gmail.com>
2015-08-25 10:35:42 +0800

    modify sql files

--- Server/Bin/dbsql/logdb/log_robotcoin.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `log_robotcoin` (
-  `robotZoneOffset1` bigint(20) NOT NULL,
-  `robotZoneOffset2` bigint(20) DEFAULT NULL,
-  `robotZoneOffset3` bigint(20) DEFAULT NULL,
-  `robotZoneOffset4` bigint(20) DEFAULT NULL,
-  `robotTotalOffset` bigint(20) DEFAULT NULL,
-  `logTime` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`robotZoneOffset1`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
